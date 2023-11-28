5a4318b8641e57a309b75b61f69c58aa00fe1caf
saakhani <muhammad.saad.002002@gmail.com>
2023-11-29 00:31:14 +0500

    added functionality to Event page

--- frontend/src/EventPage.js
+++ frontend/src/EventPage.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState, forwardRef } from 'react';
+import DatePicker from 'react-datepicker'; // Assuming you're using react-datepicker
 import images from "./images.js";
 import "./EventPage.css";
+import './custom-datepicker.css';
 
 import Header from "./Header.js";
 
 const EventPage = () => {
-
     const eventDetails ={
         title: "ABC Event",
         venue: "XYZ Venue",
@@ -14,6 +15,44 @@ const EventPage = () => {
         imgSrc: "https://dummyimage.com/600x400/bdbdbd/595959"
       };
 
+    const [selectedDate, setSelectedDate] = useState('');
+    const [selectedTime, setSelectedTime] = useState('');
+    const [ticketType, setTicketType] = useState('');
+    const [ticketPrice, setTicketPrice] = useState(0);
+    const [numTickets, setNumTickets] = useState(1);
+    
+    // Assuming these are your event times
+    // const eventTimes = ['10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '6:00 PM'];
+    const eventTimes = [];
+    
+    // Ticket prices
+    const prices = {
+        'VIP': 100, // Example price for VIP
+        'General': 50 // Example price for General
+    };
+
+    const DateCustomInput = forwardRef(({ value, onClick }, ref) => (
+        <button className="date-custom-input" onClick={onClick} ref={ref}>
+          {value || 'Select Date'}
+        </button>
+      ));
+    
+    const handleTimeSelect = (time) => {
+        setSelectedTime(time);
+    };
+    
+    const handleTicketTypeChange = (e) => {
+        setTicketType(e.target.value);
+        setTicketPrice(prices[e.target.value]);
+    };
+    
+    const handleCheckout = () => {
+        // Implement checkout logic
+        console.log("Checking out", {selectedDate, selectedTime, ticketType, numTickets});
+    };
+    
+    
+
 
     return(
         <div className="event-page">
@@ -21,7 +60,8 @@ const EventPage = () => {
                 <Header />
             </div>
             <h1>Event Details</h1>
-            <div className="event-details">
+            <div className='event-body'>
+                <div className="event-details">
                 <img src = {eventDetails.imgSrc} className = "event-image" 
                 alt="Event Image" height={400} width={600}/>
                 <h2 className="event-name">
@@ -36,6 +76,37 @@ const EventPage = () => {
                 <p className="event-description">
                     {eventDetails.description}
                 </p>
+                </div>
+                <div className="book-event">
+                    <h3>select date and time</h3>
+                    <div className="date-and-time">
+                    <div className = "date-picker">
+                        <DatePicker 
+                            selected={selectedDate}
+                            onChange={date => setSelectedDate(date)}
+                            dateFormat="MMMM d, yyyy"
+                            minDate={new Date()}
+                            customInput={<DateCustomInput />}
+                        />
+                    </div>
+                <div className="time-selector">
+                    {eventTimes == [] ? eventTimes.map(time => (
+                        <button key={time} onClick={() => handleTimeSelect(time)} className={selectedTime === time ? 'selected' : 'deselected'}>
+                            {time}
+                        </button>
+                    )): <div className='no-times'>no timings found for selected date</div>
+                }
+                </div>
+                        </div>
+
+                <select value={ticketType} onChange={handleTicketTypeChange}>
+                    <option value="">Select Ticket Type</option>
+                    <option value="VIP">VIP - ${prices.VIP}</option>
+                    <option value="General">General - ${prices.General}</option>
+                </select>
+                <input type="number" value={numTickets} onChange={(e) => setNumTickets(e.target.value)} min="1" />
+                <button className='checkout-button' onClick={handleCheckout}>checkout</button>
+                </div>
             </div>
         </div>
     )
