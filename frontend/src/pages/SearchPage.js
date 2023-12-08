b872a09609763eec03812eef5d6f33c445ecb75a
saakhani <muhammad.saad.002002@gmail.com>
2023-12-08 16:01:01 +0500

    Cleanup

--- frontend/src/pages/SearchPage.js
+++ frontend/src/pages/SearchPage.js
@@ -4,19 +4,6 @@ import SearchResultCard from "../components/SearchResultCard.js";
 import Header from '../components/Header.js';
 import { useParams } from "react-router-dom";
 
-// Import images statically
-// import artExhibitionImage from '../assets/event-images/art_exhibition.jpg';
-// import techConferenceImage from '../assets/event-images/tech_conference.jpg';
-// import foodFestivalImage from '../assets/event-images/food_festival.jpg';
-// import comedyNightImage from '../assets/event-images/comedy_night.jpg';
-// import fashionShowImage from '../assets/event-images/fashion_show.jpg';
-// import scienceExpoImage from '../assets/event-images/science_expo.jpg';
-// import fitnessExpoImage from '../assets/event-images/fitness_expo.jpg';
-// import craftFairImage from '../assets/event-images/craft_fair.jpg';
-// import gardenPartyImage from '../assets/event-images/garden_party.jpg';
-// import wellnessWorkshopImage from '../assets/event-images/wellness_workshop.jpg';
-
-
 const SearchPage = () => {
   const params = useParams();
   const [searchQuery] = useState(params.SearchBoxQuery);
@@ -33,53 +20,11 @@ const SearchPage = () => {
       .then(response => response.json())
       .then(data => {
         const searchData = data.map(item => {
-          // let imgSrc;
-
-          // // Use a switch or if-else statement to select the correct image import
-          // switch (item.event_name.toLowerCase()) {
-          //   case 'art exhibition':
-          //     imgSrc = artExhibitionImage;
-          //     break;
-          //   case 'tech conference':
-          //     imgSrc = techConferenceImage;
-          //     break;
-          //   case 'food festival':
-          //     imgSrc = foodFestivalImage;
-          //     break;
-          //   case 'comedy night':
-          //     imgSrc = comedyNightImage;
-          //     break;
-          //   case 'fashion show':
-          //     imgSrc = fashionShowImage;
-          //     break;
-          //   case 'science expo':
-          //     imgSrc = scienceExpoImage;
-          //     break;
-          //   case 'fitness expo':
-          //     imgSrc = fitnessExpoImage;
-          //     break;
-          //   case 'craft fair':
-          //     imgSrc = craftFairImage;
-          //     break;
-          //   case 'garden party':
-          //     imgSrc = gardenPartyImage;
-          //     break;
-          //   case 'wellness workshop':
-          //     imgSrc = wellnessWorkshopImage;
-          //     break;
-          //   // Add more cases as needed
-
-          //   default:
-          //     imgSrc = ''; // Default image if no match
-          //     break;
-          // }
-
           return {
             eventID: item.event_id,
             title: item.event_name,
             venue: item.venue,
             description: item.description,
-            // imgSrc: imgSrc,
           };
         });
 
